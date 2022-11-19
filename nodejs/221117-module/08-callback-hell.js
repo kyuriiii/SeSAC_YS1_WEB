fd4e2d0e872600791709a229953a18c64dd9d0a0
kyuriiii <kyuri99@naver.com>
2022-11-19 12:24:38 +0900

    feat: 221117 callback lesson

--- nodejs/221117-module/08-callback-hell.js
+++ nodejs/221117-module/08-callback-hell.js
@@ -1,4 +1,3 @@
-/*
 function login(id, pw, cb) {
     setTimeout(function(){
         console.log("사용자 입장");
@@ -24,19 +23,3 @@ login('kim', '1234', function(user){
         });
     });
 });
-*/
-function login(id, pw, cb) {
-    console.log("사용자 입장");
-    return id;
-}
-function getVideo(id, cb) {
-    console.log( id + "의 비디오 리스트");
-    return ['아바타', '라라랜드'];
-}
-function getDetail(video, cb) {
-    return "비디오 제목 : " + video;
-}
-let user = login('kim', '1234');
-let videos = getVideo(user);
-let msg = getDetail(videos[0]);
-console.log(msg);
