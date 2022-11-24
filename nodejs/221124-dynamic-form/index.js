ea0c95bcc5b00c31f1d163ca8cf31bcd01a2ef4a
kyuriiii <kyuri99@naver.com>
2022-11-24 18:26:34 +0900

    feat: 221124 practice 35 ajax, fetch answer

--- nodejs/221124-dynamic-form/index.js
+++ nodejs/221124-dynamic-form/index.js
@@ -20,9 +20,9 @@ app.post("/form", function(req,res){
 
 app.post("/login", function(req,res){
     if ( req.body.id == "1" && req.body.pw == "1234" ){
-        res.send("<p style='color:blue'>로그인 성공</p>");
+        res.send({msg: "<p style='color:blue'>로그인 성공</p>"});
     } else {
-        res.send("<p style='color:red;'>로그인 실패 ( 아이디 또는 비밀번호가 틀렸습니다.)");
+        res.send({msg:"<p style='color:red;'>로그인 실패 ( 아이디 또는 비밀번호가 틀렸습니다.)"});
     }
 })
 app.listen(port, ()=>{
