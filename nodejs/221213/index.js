52ebf1d1cd8b7db7168419566583171fb5897586
kyuriiii <47288015+kyuriiii@users.noreply.github.com>
2022-12-13 19:10:16 +0900

    feat: 221213 env

---
+++ nodejs/221213/index.js
@@ -0,0 +1,14 @@
+// dotenv = 환경변수
+// 
+
+console.log( process.env.PORT);
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: './common.env'});
+dotenv.config({path: path.join(__dirname, './common.env' )});
+
+console.log( process.env.PORT);
+console.log( process.env );
+
+// Figma
+// Postman
