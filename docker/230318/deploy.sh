e62ed611d2e1f6322a4f4b90de3917ab1b0875ab
kyuriiii <kyuri99@naver.com>
2023-05-18 12:51:31 +0900

    feat: Do deploy test

---
+++ docker/230318/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ 
+# Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
+EXIST_BLUE=$(docker-compose -p nextjs-docker-blue -f docker-compose.blue.yml ps | grep Up)
+ 
+# 컨테이너 스위칭
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue up"
+    docker-compose -p nextjs-docker-blue -f docker-compose.blue.yml up -d
+    BEFORE_COMPOSE_COLOR="green"
+    AFTER_COMPOSE_COLOR="blue"
+else
+    echo "green up"
+    docker-compose -p nextjs-docker-green -f docker-compose.green.yml up -d
+    BEFORE_COMPOSE_COLOR="blue"
+    AFTER_COMPOSE_COLOR="green"
+fi
+ 
+sleep 10
+ 
+# 새로운 컨테이너가 제대로 떴는지 확인
+EXIST_AFTER=$(docker-compose -p nextjs-docker-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
+if [ -n "$EXIST_AFTER" ]; then
+    # 이전 컨테이너 종료
+    docker-compose -p nextjs-docker-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
+    echo "$BEFORE_COMPOSE_COLOR down"
+fi
--- docker/230318/index.js
+++ docker/230318/index.js
@@ -1,11 +1,40 @@
 // index.js
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req,res) => {
-	res.send(req.query.name);
+app.get("/deploy", (req, res) => {
+  res.send(req.query.name);
+});
+app.get("/deploy/hi", (req, res) => {
+  res.send("hihihi");
+});
+app.get("/deploy/end", (req, res) => {
+  const Sequelize = require("sequelize");
+
+  const config = {
+    username: "codingon",
+    password: "codingon",
+    database: "codingon",
+    host: "db-lesson",
+    dialect: "mysql",
+  };
+
+  const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+  sequelize
+    .sync()
+    .then(() => {
+      console.log("DB connect success");
+    })
+    .catch((err) => {
+      throw err;
+    });
 });
 
-app.listen(8000, ()=>{
-	console.log("Server is open, port : 8000");
+app.listen(8000, () => {
+  console.log("Server is open, port : 8000");
 });
